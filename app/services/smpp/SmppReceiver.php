c8a7efda43427a5994253a64a4dfc2e7801ffd1e
Husain-Zreik <husain.zreik1@gmail.com>
2024-03-13 10:29:06 +0200

    add smpp service

---
+++ app/services/smpp/SmppReceiver.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Services\Smpp;
+
+use Illuminate\Support\Facades\Log;
+use SmppClient;
+use SmppDeliveryReceipt;
+use SocketTransport;
+
+class SmppReceiver
+{
+    protected $transport, $client, $transmitter;
+
+    public function start()
+    {
+        $this->connect();
+        $this->readSms();
+    }
+
+    protected function connect()
+    {
+        $this->transport = new SocketTransport([config('smpp.smpp_service')], config('smpp.smpp_port'));
+        $this->transport->setRecvTimeout(30000);
+        $this->transport->setSendTimeout(30000);
+
+        // Create client
+        $this->client = new SmppClient($this->transport);
+
+        // Activate binary hex-output of server interaction
+        $this->client->debug = true;
+        $this->transport->debug = true;
+
+        // Open the connection
+        $this->transport->open();
+
+        // Bind receiver
+        $this->client->bindReceiver(config('smpp.smpp_receiver_id'), config('smpp.smpp_receiver_id'));
+    }
+
+
+    protected function disconnect()
+    {
+        if (isset($this->transport) && $this->transport->isOpen()) {
+            if (isset($this->client)) {
+                try {
+                    $this->client->close();
+                } catch (\Exception $e) {
+                    $this->transport->close();
+                }
+            } else {
+                $this->transport->close();
+            }
+        }
+    }
+    // protected function keepAlive()
+    // {
+    //     $this->client->enquireLink();
+    //     $this->client->respondEnquireLink();
+    // }
+
+    protected function keepAlive()
+    {
+        $this->client->enquireLink();
+        $this->client->read(); // Read the EnquireLink response
+    }
+
+
+
+    protected function readSms()
+    {
+        $time_start = microtime(true);
+        $endtime = $time_start + 120; // 2 minutes
+        $lastTime = 0;
+
+        do {
+            $res = $this->client->readSMS();
+            if ($res) {
+                try {
+                    if ($res instanceof SmppDeliveryReceipt) {
+                        // Handle delivery receipts
+                    } else {
+                        $from = $res->source->value;
+                        $to = $res->destination->value;
+                        $message = $res->message;
+                        // Process received SMS message
+                        dd($from, $message, $to);
+                    }
+                } catch (\Exception $e) {
+                    // Handle specific exceptions or log general errors
+                    Log::error('Error while processing SMS: ' . $e->getMessage());
+                }
+                $lastTime = time();
+            } else {
+                $this->client->respondEnquireLink();
+                $this->client->read();
+            }
+            // Keep connection alive every 30 seconds
+            if (time() - $lastTime >= 30) {
+                $this->keepAlive();
+                $lastTime = time();
+            }
+        } while ($endtime > microtime(true));
+    }
+}
